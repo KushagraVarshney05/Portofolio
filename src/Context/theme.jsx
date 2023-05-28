c691f8a2c4a9751603223c26b18f864430c7eff3
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-05-28 23:53:06 +0530

    updated dark mode

--- src/Context/theme.jsx
+++ src/Context/theme.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import PropTypes from "prop-types";
 
 const ThemeContext = React.createContext();
+// localStorage.setItem("theme", "light");
 
 const ThemeProvider = ({ children }) => {
-  const [themename, setthemename] = useState("light");
+  const [themename, setthemename] = useState(localStorage.theme);
   const toggeltheme = () => {
     themename === "light" ? setthemename("dark") : setthemename("light");
   };
+  useEffect(() => {
+    const root = window.document.documentElement;
+    // root.classList.remove();
+    root.classList.add(themename);
+    localStorage.setItem("theme", themename);
+  }, [themename, toggeltheme]);
 
   return (
     <ThemeContext.Provider value={[{ themename, toggeltheme }]}>
